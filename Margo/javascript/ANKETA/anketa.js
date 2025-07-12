fac63ca1bdd0f9a7ba23f35e620c53812578d7f0
Margokitaeva <147771338+Margokitaeva@users.noreply.github.com>
2025-07-12 10:16:47 +0200

    js task 1 fixed name issue

--- Margo/javascript/ANKETA/anketa.js
+++ Margo/javascript/ANKETA/anketa.js
@@ -2,19 +2,19 @@ function isValidFio(str) {
     return typeof str === "string" && str
 }
 
-let name = prompt('Введите Ваше имя', 'Серафима');
-while(!isValidFio(name)) {
-    name = prompt(`Имя не может быть пустым и должно быть текстом. Вы ввели ${name}.\nВведите Ваше имя`, 'Серафима');
+let userName = prompt('Введите Ваше имя', 'Серафима');
+while(!isValidFio(userName)) {
+    userName = prompt(`Имя не может быть пустым и должно быть текстом. Вы ввели ${name}.\nВведите Ваше имя`, 'Серафима');
 }
 
-let surname = prompt("Введите Вашу фамилию", "Долмацкая");
-while (!isValidFio(surname)) {
-    surname = prompt(`Фамилия не может быть пустой и должна быть текстом. Вы ввели ${surname}.\nВведите Вашу фамилию`, "Долмацкая");
+let userSurname = prompt("Введите Вашу фамилию", "Долмацкая");
+while (!isValidFio(userSurname)) {
+    userSurname = prompt(`Фамилия не может быть пустой и должна быть текстом. Вы ввели ${surname}.\nВведите Вашу фамилию`, "Долмацкая");
 }
 
-let lastName = prompt("Введите Ваше отчество", "Платоновна");
-while (!isValidFio(lastName)) {
-    lastName = prompt(`Отчество не может быть пустым и должно быть текстом. Вы ввели ${lastName}.\nВведите Ваше отчество`, "Платоновна");
+let userLastName = prompt("Введите Ваше отчество", "Платоновна");
+while (!isValidFio(userLastName)) {
+    userLastName = prompt(`Отчество не может быть пустым и должно быть текстом. Вы ввели ${lastName}.\nВведите Ваше отчество`, "Платоновна");
 }
 
 let ageString = prompt("Введите Ваш возраст. Возраст должен быть целым числом больше 0", "17");
@@ -23,6 +23,7 @@ while (isNaN(age) || !Number.isInteger(age) || age < 1) {
     ageString = prompt(`Возраст должен быть целым числом больше 0. Вы ввели ${age}\nВведите Ваш возраст`, "17");
     age = Number(ageString);
 }
+
 
 let ageInDays = age * 365;
 let ageInFiveYears = age + 5;
@@ -39,6 +40,11 @@ if (!isMan && age >= 58)
 let sex = isMan ? "мужской" : "женский";
 let pension = isOld ? "да" : "нет";
 
-text = `ваше ФИО: ${surname} ${name} ${lastName}\nваш возраст в годах: ${age}\nваш возраст в днях: ${ageInDays}\nчерез 5 лет вам будет: ${ageInFiveYears}\nваш пол: ${sex}\nвы на пенсии: ${pension}`
+text = `ваше ФИО: ${userSurname} ${userName} ${userLastName}
+ваш возраст в годах: ${age}
+ваш возраст в днях: ${ageInDays}
+через 5 лет вам будет: ${ageInFiveYears}
+ваш пол: ${sex}
+вы на пенсии: ${pension}`
 alert(text);
 
